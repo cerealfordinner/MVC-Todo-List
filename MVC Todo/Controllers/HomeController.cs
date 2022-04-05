a04357024211a306043fab75da86f7ef557070e7
cerealfordinner <cerealfordinnerx@gmail.com>
2022-04-04 22:26:46 -0400

    Add project files.

---
+++ MVC Todo/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using MVC_Todo.Models;
+
+namespace MVC_Todo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        internal TodoViewModel GetAllTodos()
+        {
+            List<TodoItem> todoList = new();
+
+            using (SqliteConnection con = 
+                new SqliteConnection("Data Source=db.sqlite"))
+            {
+                using (var tableCmd = con.CreateCommand())
+                {
+                    con.Open();
+                    tableCmd.CommandText = "SELECTR * FROM todo";
+
+                    using (var reader = tableCmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                todoList.Add(new TodoItem
+                                {
+                                    Id = reader.GetInt32(0),
+                                    Name = reader.GetString(1)
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public RedirectResult Insert(TodoItem todo)
+        {
+            using (SqliteConnection con =
+                new SqliteConnection("Data Source=db.sqlite"))
+            {
+                using (var tableCmd = con.CreateCommand())
+                {
+                    con.Open();
+                    tableCmd.CommandText = $"INSERT INTO todo (name) VALUES ('{todo.Name}')";
+                    try
+                    {
+                        tableCmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+            return Redirect("https://localhost:7082");
+        }
+    }
+}
---
+++ MVC Todo/Models/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace MVC_Todo.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
